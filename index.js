12580abbf41c04c4e37605ab86713508e71ba3cb
Stanislav <stanislav.pogorskii@gmail.com>
2023-10-25 11:43:39 +0400

    temp fix

--- index.js
+++ index.js
@@ -7,10 +7,13 @@ const app = express();
 // Connect MongoDB
 const mongoose = require("mongoose");
 mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(
+    "mongodb+srv://staspogorskii:somepass@cluster0.tpz2rdo.mongodb.net/?retryWrites=true",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
   .then(() => console.log("Connected to MongoDB"))
   .catch((err) => console.error("Failed to connect to MongoDB:", err));
 
